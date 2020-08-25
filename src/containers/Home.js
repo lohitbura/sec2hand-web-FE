fe04e64958897ba6eb8d2e5bcc61ee7831c3aa4f
iamharshkumar <hk7206210@gmail.com>
2020-08-25 22:53:24 +0530

    add ui

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -44,75 +44,166 @@ class HomepageLayout extends React.Component {
     render() {
         const {products} = this.state;
         return (
-            <Container>
-                <CarouselProvider
-                    naturalSlideWidth={110}
-                    naturalSlideHeight={50}
-                    totalSlides={3}
-                    isPlaying={true}
-                    interval={3000}
-                >
-                    <Slider>
-                        <Slide index={1}><img
-                            style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
-                            src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
-                            alt="First slide"/></Slide>
-                        }
-                        <Slide index={2}><img
-                            style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
-                            src='https://bd.gaadicdn.com/processedimages/revolt-motors/rv-400/source/m_rv-400_11560855440.jpg?tr=w-360'
-                            alt="First slide"/></Slide>
-                        }
-                        <Slide index={3}><img
-                            style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
-                            src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
-                            alt="First slide"/></Slide>
-                        }
-                    </Slider>
-                </CarouselProvider>
+            <div>
 
-                <Grid style={{marginTop: '100px'}} container columns={3}>
-                    {
-                        products.map(product => {
-                            return (
-                                <Grid.Column>
-                                    <Link to={`/product/${product.id}`}>
-                                        <Card>
-                                            <Image
-                                                src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
-                                                wrapped ui={false}/>
-                                            <Card.Content>
-                                                <Card.Header>
-                                                    <Link to={`/product/${product.id}`}>
-                                                        {product.model}
-                                                    </Link>
-                                                </Card.Header>
-                                                <Card.Meta>Joined in 2016</Card.Meta>
-                                                <Card.Description>
-                                                    ₹ {product.price}
-                                                </Card.Description>
-                                            </Card.Content>
-                                        </Card>
-                                    </Link>
-                                </Grid.Column>
-
-                            )
-                        })
-                    }
-
-                </Grid>
-                <div style={{textAlign: "center", marginTop: "50px"}}>
-                    <Pagination
-                        boundaryRange={0}
-                        defaultActivePage={1}
-                        ellipsisItem={null}
-                        firstItem={null}
-                        lastItem={null}
-                        siblingRange={1}
-                        totalPages={10}
-                    />
+                <div className="banner header-text">
+                    <div className="owl-banner owl-carousel">
+                        <div className="banner-item-01">
+                            <div className="text-content">
+                                <h4>Find your car today!</h4>
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                            </div>
+                        </div>
+                        <div className="banner-item-02">
+                            <div className="text-content">
+                                <h4>Fugiat Aspernatur</h4>
+                                <h2>Laboriosam reprehenderit ducimus</h2>
+                            </div>
+                        </div>
+                        <div className="banner-item-03">
+                            <div className="text-content">
+                                <h4>Saepe Omnis</h4>
+                                <h2>Quaerat suscipit unde minus dicta</h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </Container>
+
+
+                <div className="latest-products">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="section-heading">
+                                    <h2>Featured Products</h2>
+                                    <a href="cars.html">view more <i className="fa fa-angle-right"></i></a>
+                                </div>
+                            </div>
+                            {
+                                products.map(product => {
+                                    return (
+                                        <div className="col-lg-4 col-md-6">
+                                            <div className="product-item">
+                                                <a href="car-details.html"><img
+                                                    src={`${product.image}`}
+                                                    alt=""/></a>
+                                                <div className="down-content">
+                                                    <a href="car-details.html"><h4>
+                                                        <Link to={`/product/${product.id}`}>
+                                                            {product.model}
+                                                        </Link>
+                                                    </h4></a>
+
+                                                    <h6><small>
+                                                        <del> ₹ {product.price}</del>
+                                                    </small> ₹ {product.price}
+                                                    </h6>
+
+                                                    {/*<p>190 hp &nbsp;/&nbsp; Petrol &nbsp;/&nbsp; 2008 &nbsp;/&nbsp; Used*/}
+                                                    {/*    vehicle</p>*/}
+
+                                                    <small>
+                                                        <strong title="Author"><i className="fa fa-dashboard"></i>
+                                                            {product.km}km
+                                                        </strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                                        <strong title="Author"><i
+                                                            className="fa fa-cube"></i> {product.color}
+                                                        </strong>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                        <strong title="Views"><i
+                                                            className="fa fa-cog"></i> Manual</strong>
+                                                    </small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                        </div>
+                        <div style={{textAlign: "center", marginTop: "50px"}}>
+                            <Pagination
+                                boundaryRange={0}
+                                defaultActivePage={1}
+                                ellipsisItem={null}
+                                firstItem={null}
+                                lastItem={null}
+                                siblingRange={1}
+                                totalPages={10}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            // <Container>
+            //     <CarouselProvider
+            //         naturalSlideWidth={110}
+            //         naturalSlideHeight={50}
+            //         totalSlides={3}
+            //         isPlaying={true}
+            //         interval={3000}
+            //     >
+            //         <Slider>
+            //             <Slide index={1}><img
+            //                 style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
+            //                 src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+            //                 alt="First slide"/></Slide>
+            //             }
+            //             <Slide index={2}><img
+            //                 style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
+            //                 src='https://bd.gaadicdn.com/processedimages/revolt-motors/rv-400/source/m_rv-400_11560855440.jpg?tr=w-360'
+            //                 alt="First slide"/></Slide>
+            //             }
+            //             <Slide index={3}><img
+            //                 style={{'objectFit': 'cover', 'height': '100%', 'width': '100%'}}
+            //                 src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+            //                 alt="First slide"/></Slide>
+            //             }
+            //         </Slider>
+            //     </CarouselProvider>
+            //
+            //     <Grid style={{marginTop: '100px'}} container columns={3}>
+            //         {
+            //             products.map(product => {
+            //                 return (
+            //                     <Grid.Column>
+            //                         <Link to={`/product/${product.id}`}>
+            //                             <Card>
+            //                                 <Image
+            //                                     src='https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+            //                                     wrapped ui={false}/>
+            //                                 <Card.Content>
+            //                                     <Card.Header>
+            //                                         <Link to={`/product/${product.id}`}>
+            //                                             {product.model}
+            //                                         </Link>
+            //                                     </Card.Header>
+            //                                     <Card.Meta>Joined in 2016</Card.Meta>
+            //                                     <Card.Description>
+            //                                         ₹ {product.price}
+            //                                     </Card.Description>
+            //                                 </Card.Content>
+            //                             </Card>
+            //                         </Link>
+            //                     </Grid.Column>
+            //
+            //                 )
+            //             })
+            //         }
+            //
+            //     </Grid>
+            //     <div style={{textAlign: "center", marginTop: "50px"}}>
+            //         <Pagination
+            //             boundaryRange={0}
+            //             defaultActivePage={1}
+            //             ellipsisItem={null}
+            //             firstItem={null}
+            //             lastItem={null}
+            //             siblingRange={1}
+            //             totalPages={10}
+            //         />
+            //     </div>
+            // </Container>
         )
     }
 }
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -23,7 +23,7 @@ class CustomLayout extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if(prevProps.token !== this.props.token){
+        if (prevProps.token !== this.props.token) {
             let headers = {
                 Authorization: `Token ${localStorage.getItem('token')}`
             };
@@ -38,49 +38,103 @@ class CustomLayout extends React.Component {
         const {username} = this.state;
         return (
             <div>
-                <Menu inverted>
-                    <Container>
-                        <Link to="/">
-                            <Menu.Item header>Home</Menu.Item>
-                        </Link>
-                        <Link to="/review">
-                            <Menu.Item header>Review</Menu.Item>
-                        </Link><Link to="/dealer">
-                        <Menu.Item header>Find dealer</Menu.Item>
-                    </Link>
-                        {authenticated ? (
-                            <Menu.Menu position="right">
-                                <Menu.Item header>
-                                    <Link to={`/profile/${username}`}>
-                                        Profile
-                                    </Link>
-                                </Menu.Item>
-                            </Menu.Menu>
+                <div id="preloader">
+                    <div classNameName="jumper">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
 
-                        ) : ''
-                        }
-                        {authenticated ? (
-                            <Menu.Item header onClick={() => this.props.logout()}>
-                                Logout
-                            </Menu.Item>
-                        ) : (
-                            <React.Fragment>
-                                <Menu.Menu position="right">
-                                    <Dropdown item text='Login'>
-                                        <Dropdown.Menu style={{color: 'black'}}>
-                                            <Dropdown.Item><Link style={{color: "black"}} to="/login">
-                                                Customer
-                                            </Link></Dropdown.Item>
-                                            <Dropdown.Item><Link style={{color: "black"}} to="/dealer-login">
-                                                Dealer
-                                            </Link></Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Menu.Menu>
-                            </React.Fragment>
-                        )}
-                    </Container>
-                </Menu>
+                <header className="">
+                    <nav className="navbar navbar-expand-lg">
+                        <div className="container">
+                            <a className="navbar-brand" href="index.html"><h2>Sec2 <em>Hand</em></h2></a>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarResponsive" aria-controls="navbarResponsive"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarResponsive">
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item active">
+                                        <Link className="nav-link" to="/">
+                                            Home
+                                        </Link>
+                                        <span className="sr-only">(current)</span>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/review">
+                                            Review
+                                        </Link>
+                                    </li>
+
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#"
+                                           role="button" aria-haspopup="true" aria-expanded="false">More</a>
+
+                                        <div className="dropdown-menu">
+                                            <a className="dropdown-item" href="blog.html">Blog</a>
+                                            <a className="dropdown-item" href="team.html">Team</a>
+                                            <a className="dropdown-item" href="testimonials.html">Testimonials</a>
+                                            <a className="dropdown-item" href="terms.html">Terms</a>
+                                        </div>
+                                    </li>
+
+                                    <li className="nav-item"><a className="nav-link" href="about-us.html">About Us</a>
+                                    </li>
+
+                                    <li className="nav-item"><a className="nav-link" href="contact.html">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                {/*<Menu inverted>*/}
+                {/*    <Container>*/}
+                {/*        <Link to="/">*/}
+                {/*            <Menu.Item header>Home</Menu.Item>*/}
+                {/*        </Link>*/}
+                {/*        <Link to="/review">*/}
+                {/*            <Menu.Item header>Review</Menu.Item>*/}
+                {/*        </Link><Link to="/dealer">*/}
+                {/*        <Menu.Item header>Find dealer</Menu.Item>*/}
+                {/*    </Link>*/}
+                {/*        {authenticated ? (*/}
+                {/*            <Menu.Menu position="right">*/}
+                {/*                <Menu.Item header>*/}
+                {/*                    <Link to={`/profile/${username}`}>*/}
+                {/*                        Profile*/}
+                {/*                    </Link>*/}
+                {/*                </Menu.Item>*/}
+                {/*            </Menu.Menu>*/}
+
+                {/*        ) : ''*/}
+                {/*        }*/}
+                {/*        {authenticated ? (*/}
+                {/*            <Menu.Item header onClick={() => this.props.logout()}>*/}
+                {/*                Logout*/}
+                {/*            </Menu.Item>*/}
+                {/*        ) : (*/}
+                {/*            <React.Fragment>*/}
+                {/*                <Menu.Menu position="right">*/}
+                {/*                    <Dropdown item text='Login'>*/}
+                {/*                        <Dropdown.Menu style={{color: 'black'}}>*/}
+                {/*                            <Dropdown.Item><Link style={{color: "black"}} to="/login">*/}
+                {/*                                Customer*/}
+                {/*                            </Link></Dropdown.Item>*/}
+                {/*                            <Dropdown.Item><Link style={{color: "black"}} to="/dealer-login">*/}
+                {/*                                Dealer*/}
+                {/*                            </Link></Dropdown.Item>*/}
+                {/*                        </Dropdown.Menu>*/}
+                {/*                    </Dropdown>*/}
+                {/*                </Menu.Menu>*/}
+                {/*            </React.Fragment>*/}
+                {/*        )}*/}
+                {/*    </Container>*/}
+                {/*</Menu>*/}
                 {this.props.children}
 
                 <Segment
@@ -145,26 +199,26 @@ class CustomLayout extends React.Component {
                     </Container>
                 </Segment>
             </div>
-        );
+    );
     }
-}
+    }
 
-const mapStateToProps = state => {
-    return {
+    const mapStateToProps = state => {
+        return {
         authenticated: state.auth.token !== null,
-        token:state.auth.token
+        token: state.auth.token
     };
-};
+    };
 
-const mapDispatchToProps = dispatch => {
-    return {
+    const mapDispatchToProps = dispatch => {
+        return {
         logout: () => dispatch(logout())
     };
-};
+    };
 
-export default withRouter(
+    export default withRouter(
     connect(
-        mapStateToProps,
-        mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
     )(CustomLayout)
-);
+    );
