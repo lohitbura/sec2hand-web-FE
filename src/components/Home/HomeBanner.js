f1bd5e5c85155497a55bddc7e49db5a322906509
Pankaj Verma <pv62012@gmail.com>
2021-12-29 03:01:01 +0530

    home responsive

--- src/components/Home/HomeBanner.js
+++ src/components/Home/HomeBanner.js
@@ -5,89 +5,126 @@ import * as ImIcons from "react-icons/im";
 import { Grid } from "@material-ui/core";
 import Uncle from "../../assets/images/uncle1.png";
 import background from "../../assets/images/background-top.png";
+import "./HomeStyle.css";
 import { Link } from "react-router-dom";
 export default function HomeBanner() {
   return (
-    <div style={{ backgroundImage: `url(${background})`, height: "100vh" }}>
-      <Grid container spacing={0}>
-        <Grid lg={6} xs={12} item>
-          <div className="bannerPadding" style={{ padding: "14% 16%" }}>
-            <p
-              style={{
-                fontSize: '300%',
-                color: "#5B1C03",
-                fontWeight: "600",
-                height: 18,
-                width: 400,
-              }}
-            >
-              SECOND HAND
-            </p>
-            <p
-              style={{
-                fontSize: 52,
-                color: "#5B1C03",
-                fontWeight: "400",
-                height: 26,
-              }}
-            >
-              {" "}
-              मतलब
-            </p>
-            <p style={{ fontSize: 50, color: "#5B1C03", fontWeight: "600" }}>
-              SEC2HAND
-            </p>
+    <div className="home_banner_main_div">
+      <div className="home_banner_left_div">
+        <div
+          className="home_banner_left_details "
+          style={{ padding: "14% 16%" }}
+        >
+          <p
+            style={{
+              fontSize: "300%",
+              color: "#5B1C03",
+              fontWeight: "600",
+              height: 18,
+              width: 400,
+            }}
+          >
+            SECOND HAND
+          </p>
+          <p
+            style={{
+              fontSize: 52,
+              color: "#5B1C03",
+              fontWeight: "400",
+              height: 26,
+            }}
+          >
+            {" "}
+            मतलब
+          </p>
+          <p style={{ fontSize: 50, color: "#5B1C03", fontWeight: "600" }}>
+            SEC2HAND
+          </p>
 
-            <button
-              style={{
-                padding: "6px 22px",
-                backgroundColor: "#5B1C03",
-                color: "#E6B05B",
-                borderRadius: 20,
-                fontSize: 20,
-                border: "none",
-                fontWeight: "bold",
-              }}
-            >
-              BUY
-            </button>
+          <button className="home_banner_button">BUY</button>
 
-            <Link to="/product-create">
-              <button
-                style={{
-                  padding: "6px 22px",
-                  backgroundColor: "#5B1C03",
-                  color: "#E6B05B",
-                  borderRadius: 20,
-                  fontSize: 20,
-                  border: "none",
-                  fontWeight: "bold",
-                  marginLeft: 24,
-                }}
-              >
-                SELL
-              </button>
-            </Link>
-          </div>
-        </Grid>
-
-        <Grid lg={6} xs={12} item>
-          <img className="bannerImageWidth" src={Uncle} style={{ width: "100%", height: "100%" }} />
-        </Grid>
-      </Grid>
-
-      {/*  <div className="main-container">
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="../../../assets/images/slider1.png"
-              alt="First slide"
-            />
-          </Carousel.Item>
-        </Carousel>
-      </div> */}
+          <Link to="/product-create">
+            <button className="home_banner_button">SELL</button>
+          </Link>
+        </div>
+      </div>
+      <div className="home_banner_right_div">
+        <img className="bannerImageWidth" src={Uncle} />
+      </div>
     </div>
   );
 }
 //banner
+
+{
+  /* <Grid container spacing={0}>
+<Grid lg={6} xs={12} item>
+  <div className="bannerPadding" style={{ padding: "14% 16%" }}>
+    <p
+      style={{
+        fontSize: "300%",
+        color: "#5B1C03",
+        fontWeight: "600",
+        height: 18,
+        width: 400,
+      }}
+    >
+      SECOND HAND
+    </p>
+    <p
+      style={{
+        fontSize: 52,
+        color: "#5B1C03",
+        fontWeight: "400",
+        height: 26,
+      }}
+    >
+      {" "}
+      मतलब
+    </p>
+    <p style={{ fontSize: 50, color: "#5B1C03", fontWeight: "600" }}>
+      SEC2HAND
+    </p>
+
+    <button
+      style={{
+        padding: "6px 22px",
+        backgroundColor: "#5B1C03",
+        color: "#E6B05B",
+        borderRadius: 20,
+        fontSize: 20,
+        border: "none",
+        fontWeight: "bold",
+      }}
+    >
+      BUY
+    </button>
+
+    <Link to="/product-create">
+      <button
+        style={{
+          padding: "6px 22px",
+          backgroundColor: "#5B1C03",
+          color: "#E6B05B",
+          borderRadius: 20,
+          fontSize: 20,
+          border: "none",
+          fontWeight: "bold",
+          marginLeft: 24,
+        }}
+      >
+        SELL
+      </button>
+    </Link>
+  </div>
+</Grid>
+
+<Grid lg={6} xs={12} item>
+  <img
+    className="bannerImageWidth"
+    src={Uncle}
+    style={{ width: "100%", height: "100%" }}
+  />
+</Grid>
+</Grid> */
+}
